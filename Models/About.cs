51ed33a0171eadb0a9b5110d74352700fcda4ef3
MSI <MSI@DESKTOP-8EEL6TR>
2024-05-23 22:14:57 -0500

    Agregar archivos de proyecto.

---
+++ Models/About.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Models;
+
+internal class About
+{
+    public string Title => AppInfo.Name;
+    public string Version => AppInfo.VersionString;
+    public string MoreInfoUrl => "https://aka.ms/maui";
+    public string Message => "Nuestro cerebro es como un procesador";
+    public string PersonalDescription => "Me refiero a que yo puedo realizar muchas cosas gracias a mi cerebro" +
+        ", ya que es como un prpcesador de una computadora capaz de realizar cualquier actividad que me proponga";
+}
